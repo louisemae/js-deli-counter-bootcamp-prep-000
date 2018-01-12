4d30c8a7cfbc59fa262a03ea35b9f199a2cb070e
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-12 20:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeliLine) {
     line = line + " empty.";
   }
   else {
-    line = line + `: 1. ${katzDeliLine[1]}`;
+    line = line + `: 1. ${katzDeliLine[0]}`;
     for (var i = 0; i<katzDeliLine.length-1; i++) {
       line = line + `, ${i+1}. ${katzDeliLine[i]}`;
     }
